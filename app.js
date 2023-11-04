8ce9f21a88b1f2c4a27123e6a625fdbb414fc552
Maximilian <scientificresearch309@gmail.com>
2023-11-04 22:56:09 +0100

    Solution completed!

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const path = require("path");
 const express = require("express");
 const mainRoutes = require("./routes/index");
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(mainRoutes);
 
