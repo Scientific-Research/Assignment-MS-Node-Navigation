1a40a65f52a87fa7936e7d181979c209ad57c368
Maximilian <scientificresearch309@gmail.com>
2023-11-04 22:34:14 +0100

    "/" and "/users" are working well!

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const mainRoutes = require("./routes/index");
 
 const app = express();
+
+app.use(mainRoutes);
 
 app.listen(3000, () => {
   console.log("Server is listening on Port 3000");
